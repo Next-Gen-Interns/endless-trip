5339cb22e042cac9605e5e7d9c897d3cee5bc285
Yagnesh874 <yagneshbhuva2@gmail.com>
2026-01-12 12:54:39 +0530

    next congif error solvr

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-<<<<<<< HEAD
-   images: {
-    domains: [
-      "images.unsplash.com",  // Unsplash images
-      "endlesstrips.in",     // Your trip images
-=======
   images: {
+    domains: [
+      "images.unsplash.com",
+      "endlesstrips.in",
+    ],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "endlesstrips.in",
         pathname: "/uploads/**",
       },
->>>>>>> 4f30aa49bd051eb3f88a4ebf5601f5a1fe1f47c7
     ],
   },
 };
