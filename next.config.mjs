3c38cb2d1397318a44b90c74036da9d1024f358a
Yagnesh874 <yagneshbhuva2@gmail.com>
2026-01-12 12:39:48 +0530

    updated code

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+   images: {
+    domains: [
+      "images.unsplash.com",  // Unsplash images
+      "endlesstrips.in",     // Your trip images
+    ],
+  },
+};
+
+export default nextConfig;
